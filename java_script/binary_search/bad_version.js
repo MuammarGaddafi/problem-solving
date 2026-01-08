b4f277185943efed1c6453982f5e94b6128d49f8
MuammarGaddafi <wessimchok@gmail.com>
2026-01-08 15:02:23 +0100

    Add files via upload

---
+++ java_script/binary_search/bad_version.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        
+        let left = 1 , right =  n 
+ 
+
+
+while ( left <= right ) {
+
+    let middle = left + Math.floor((right - left) / 2)
+
+    if (isBadVersion(middle)=== true) {
+
+        right = middle - 1 
+        middle =  left + (  Math.floor((right - left )/2)   )
+        
+    }
+
+
+
+    else if (isBadVersion(middle) === false) {
+
+        left=middle+1 
+
+    }
+
+    
+}
+
+
+    return left
+    };
+};
